0800a555c04b59a7953f276bc1b4def8977c0d38
baddo1 <benson.addo@gmfinancial.com>
2022-03-08 15:13:31 -0600

    refactor code to use functions

--- script.js
+++ script.js
@@ -2,28 +2,36 @@ var button = document.querySelector("button");
 var items = document.querySelector("input");
 var ul = document.querySelector("ul");
 
-/* Mouse Keys */
-button.addEventListener("click", function(){
-    if(items.value.length > 0){
-        console.log(items.value);
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(items.value));
-        ul.appendChild(li);
-        items.value = ""; // This would ensure placeholder resets after value entered
-    }
-    
-});
+function getTotalLength(){
+    return items.value.length;
+}
 
-/*  Added a key Event listerner to add to list when the enter key is triggered
+function createListElement(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(items.value));
+    ul.appendChild(li);
+    console.log(items.value);
+    items.value = ""; // This would ensure placeholder resets after value entered    
+}
+
+function mouseClick(){
+    if(getTotalLength() > 0){
+        createListElement()
+    } 
+}
+
+function keypressTrigger(event){
+    /*  Added a key Event listerner to add to list when  the enter key is triggered
     https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
-*/
-button.addEventListener("keypress", function(event){
+    */
     // console.log(event.which);
-    if(items.value.length > 0 && event.keyCode === 13){
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(items.value));
-        ul.appendChild(li);
-        items.value = ""; // This would ensure placeholder resets after value entered
-    }
-    
-});
+    if(getTotalLength() > 0 && event.keyCode === 13){
+        createListElement()
+    } 
+}
+
+/* Trigger Mouse events */
+button.addEventListener("click", mouseClick);
+
+/* Trigger Key Events */
+button.addEventListener("keypress", keypressTrigger);
