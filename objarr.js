1f132fea4eb15e3c378778acb6fb3f81b4e6622e
baddo1 <benson.addo@gmfinancial.com>
2022-03-07 19:02:56 -0600

    added examples of arrays and objects for FB exercise

---
+++ objarr.js
@@ -0,0 +1,25 @@
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var user_obj = {
+    username: "John",
+    password: "secret"
+};
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [{
+    username: "John",
+    password: "test1"
+}];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [{
+    username: "Michael",
+    timeline: "Been working on this object today"
+},
+{
+    username: "John",
+    timeline: "Started working on this array object lastweek"
+},
+{
+    username: "Jose",
+    timeline: "Yesterday, I picked up the wip job from John"
+}];
