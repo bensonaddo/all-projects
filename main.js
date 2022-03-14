76273bdb3251b056b5a91dd2076346fe80372044
baddo1 <benson.addo@gmfinancial.com>
2022-03-14 00:22:21 -0500

    initial include syntax

---
+++ main.js
@@ -0,0 +1,6 @@
+// ES 7 introduced includes() which verifys if something is included
+// and returns true or false
+let pets = ['cat', 'dog', 'bat'];
+
+pets.includes('dog'); //true
+pets.includes('birds'); //false since birds is not in the list of arrays
