dc9ee149c81d16c65b4b6165762d9a76bf0936ad
baddo1 <benson.addo@gmfinancial.com>
2022-03-14 00:53:52 -0500

    example of ending comma

---
+++ main.js
@@ -0,0 +1,12 @@
+// padStart adds padding to the end of the script
+// .padStart()
+
+// padEnd adds padding to the end of the script
+// .padEnd()
+
+console.log("Turtle".padStart(100));
+console.log("Turtle".padEnd(100));
+
+const fun = (a,b,c,d,) => console.log(a);
+
+fun(1,2,3,4,);
