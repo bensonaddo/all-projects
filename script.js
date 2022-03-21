64ff36d71ee73ca8d46ad9a936589d8e44cc4fca
baddo1 <benson.addo@gmfinancial.com>
2022-03-21 16:24:15 -0500

    initial commit

--- script.js
+++ script.js
@@ -8,6 +8,36 @@
 
 You may also see different behaviours with the Color widget based on what browser you use. Remember, all browsers are implemented differently. Good luck!
  */
+
+// Import lodash
+var _ = require('lodash');
+// import { without } from "lodash";
+
+// Create an array element for demonstration
+var array = [1,2,3,4,5,6,7,8];
+
+/*
+var liveServer = require("live-server");
+ 
+var params = {
+    port: 8181, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: "/", // Set root directory that's being served. Defaults to cwd.
+    open: true, // When false, it won't load your browser by default.
+    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    mount: [['/components', './node_modules']], // Mount a directory to a route.
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+liveServer.start(params);
+*/
+
+// Remove #3 from the array using without method from lodash package
+// console.log('answer:', without(array, 3));
+console.log('answer:', _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
